e814321d6e47af415174ce32d32b1e7e31112f92
Smkh4x <youssefalmona8@gmail.com>
2026-04-10 03:55:10 -0800

    i finished javascript logic

---
+++ script.js
@@ -0,0 +1,24 @@
+let RandomNumber;
+
+let userNum;
+function main(){
+
+    if(RandomNumber === userNum){
+        document.getElementById('result').innerHTML = "<h2>You win!</h2>";
+    } else if (userNum < RandomNumber){
+        document.getElementById('result').innerHTML = "<h2>ra9m dialk s4ar!</h2>";
+    } else if (userNum > RandomNumber){
+        document.getElementById('result').innerHTML = "<h2>ra9m dialk kbar!</h2>";
+    }
+}
+
+function GenerateRandomNumber(){
+    RandomNumber = Math.floor(Math.random() * 100);
+    document.getElementById(`title-button-1`).innerHTML = "db rak generateti number bda tkhman";
+}
+
+function CheckNumber(){
+    userNum = Number(document.getElementById("number-input").value);
+   main()
+
+}
